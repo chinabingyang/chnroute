30fc63091179875ea2218056ac2a399896dac034
lovelykids <lovelykids@vip.qq.com>
2023-04-26 11:34:22 +0800

    Update ros-cu-ct-cmcc.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -18,103 +18,36 @@ wget --no-check-certificate -c -O gwbn.txt https://ispip.clang.cn/gwbn_cidr.txt
 wget --no-check-certificate -c -O other.txt https://ispip.clang.cn/othernet_cidr.txt
 
 {
-echo "/ip route rule"
-
-for net in $(cat ct.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
-done
-
-for net in $(cat cu.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
-done
-
-for net in $(cat cm.txt) ; do
-  echo "add dst-address=$net action=lookup table=CMCC"
-done
-
-for net in $(cat crtc.txt) ; do
-  echo "add dst-address=$net action=lookup table=CMCC"
-done
-
-for net in $(cat cernet.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
-done
-
-for net in $(cat gwbn.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
-done
-
-for net in $(cat other.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
-done
-} > ../ros-pbr-CT-CMCC.rsc
-
-
-{
 echo "/ip firewall address-list"
 
 for net in $(cat ct.txt) ; do
-  echo "add list=dpbr-CT address=$net"
+  echo "add list=CT address=$net"
 done
 
 for net in $(cat cu.txt) ; do
-  echo "add list=dpbr-CT address=$net"
+  echo "add list=CU address=$net"
 done
 
 for net in $(cat cm.txt) ; do
-  echo "add list=dpbr-CMCC address=$net"
+  echo "add list=CMCC address=$net"
 done
 
 for net in $(cat crtc.txt) ; do
-  echo "add list=dpbr-CMCC address=$net"
+  echo "add list=CMCC address=$net"
 done
 
 for net in $(cat cernet.txt) ; do
-  echo "add list=dpbr-CT address=$net"
+  echo "add list=CT address=$net"
 done
 
 for net in $(cat gwbn.txt) ; do
-  echo "add list=dpbr-CT address=$net"
+  echo "add list=CT address=$net"
 done
 
 for net in $(cat other.txt) ; do
-  echo "add list=dpbr-CT address=$net"
+  echo "add list=CT address=$net"
 done
-} > ../ros-dpbr-CT-CMCC.rsc
-
-
-######电信联通
-{
-echo "/ip firewall address-list"
-
-for net in $(cat ct.txt) ; do
-  echo "add list=dpbr-CT address=$net"
-done
-
-for net in $(cat cu.txt) ; do
-  echo "add list=dpbr-CU address=$net"
-done
-
-for net in $(cat cm.txt) ; do
-  echo "add list=dpbr-CT address=$net"
-done
-
-for net in $(cat crtc.txt) ; do
-  echo "add list=dpbr-CT address=$net"
-done
-
-for net in $(cat cernet.txt) ; do
-  echo "add list=dpbr-CT address=$net"
-done
-
-for net in $(cat gwbn.txt) ; do
-  echo "add list=dpbr-CT address=$net"
-done
-
-for net in $(cat other.txt) ; do
-  echo "add list=dpbr-CT address=$net"
-done
-} > ../ros-dpbr-CT-CU.rsc
+} > ../ros-CT-CU-CMCC.rsc
 
 
 cd ..
