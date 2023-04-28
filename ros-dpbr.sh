20d4080c24f44c86bb98dae1a7530cab87fb2ddd
lovelykids <lovelykids@vip.qq.com>
2023-04-28 20:41:45 +0800

    Update ros-iplist.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -18,6 +18,39 @@ wget --no-check-certificate -c -O gwbn.txt https://ispip.clang.cn/gwbn_cidr.txt
 wget --no-check-certificate -c -O other.txt https://ispip.clang.cn/othernet_cidr.txt
 
 {
+echo "/ip route rule"
+
+for net in $(cat ct.txt) ; do
+  echo "add dst-address=$net action=lookup table=CT"
+done
+
+for net in $(cat cu.txt) ; do
+  echo "add dst-address=$net action=lookup table=CU"
+done
+
+for net in $(cat cm.txt) ; do
+  echo "add dst-address=$net action=lookup table=CMCC"
+done
+
+for net in $(cat crtc.txt) ; do
+  echo "add dst-address=$net action=lookup table=CMCC"
+done
+
+for net in $(cat cernet.txt) ; do
+  echo "add dst-address=$net action=lookup table=CT"
+done
+
+for net in $(cat gwbn.txt) ; do
+  echo "add dst-address=$net action=lookup table=CMCC"
+done
+
+for net in $(cat other.txt) ; do
+  echo "add dst-address=$net action=lookup table=CT"
+done
+} > ../ros-route-CT-CMCC-CU.rsc
+
+
+{
 echo "/ip firewall address-list"
 
 for net in $(cat ct.txt) ; do
@@ -41,14 +74,13 @@ for net in $(cat cernet.txt) ; do
 done
 
 for net in $(cat gwbn.txt) ; do
-  echo "add list=CT address=$net"
+  echo "add list=CMCC address=$net"
 done
 
 for net in $(cat other.txt) ; do
   echo "add list=CT address=$net"
 done
-} > ../ros-CT-CU-CMCC.rsc
-
+} > ../ros-firewall-CT-CMCC-CU.rsc
 
 cd ..
 rm -rf ./pbr
